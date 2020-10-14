a4bc8853051961df5f1575984aa342680278bbeb
KeithsCreations <khuss001@ucr.edu>
2020-10-14 04:15:02 -0700

    initial commit unique to this branch

---
+++ action_page.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h1>This is the homepage for our movie analysis website</h1>
+Request received! <br>
+Welcome!
+
+</body>
+</html>
---
+++ foop.js
@@ -0,0 +1,4 @@
+function update() {
+    document.getElementById("myImage").src = "images/perhaps.png";
+    console.log("Image perhaps.png loaded!")
+}
