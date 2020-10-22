592599744ad8ac2e9ab813f2c9f8a72c6a3cd601
Sergio Nieto <s.nieto.t@gmail.com>
2020-10-22 13:48:10 -0700

    Remove some .gitclean files and added a basic search

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,15 +1,15 @@
 // Information to reach website
 const movieAnalyzeUrl = 'localhost:3000/';
-const query;
+const query = 'movie?';
 
 // Select the page elements that will submit the form
-const inputField;
-const submit;
-const responseField;
+const inputField = document.getElementById('search');
+const submit = document.getElementById('submitMovieQuery');
+const responseField = document.getElementById('responseField');
 
 // Ajax Function
 const getMovie = () => {
-    const movieQuery;
+    const movieQuery = inputField.value;
     const endPoint = `${movieAnalyzeUrl}${query}${movieQuery}`;
 
     const xhr = new XMLHttpRequest();
@@ -17,28 +17,31 @@ const getMovie = () => {
 
     xhr.onreadystatechange = () => {
         if (xhr.readyState === XMLHttpRequest.DONE) {
-            // renderFunction
+            responseField.innerHTML = xhr.response;
         }
     };
 
-    xhr.open('GET', endPoint);
+    xhr.open('GET', '/movie');
     xhr.send();
 }
 
-$(document).ready(function(e){
-    $('.search-panel .dropdown-menu').find('a').click(function(e) {
-          e.preventDefault();
-          var param = $(this).attr("href").replace("#","");
-          var concept = $(this).text();
-          $('.search-panel span#search_concept').text(concept);
-          $('.input-group #search_param').val(param);
-         });
-    });
-var a = document.getElementByTagName('a').item(0);
-$(a).on('keyup', function(evt){
-console.log(evt);
-if(evt.keycode === 13){
+document.getElementById('submitMovieQuery').addEventListener('click', getMovie, true);
 
-alert('search?');
-} 
-}); 
+// // Code associated with dropdown menu
+// $(document).ready(function(e){
+//     $('.search-panel .dropdown-menu').find('a').click(function(e) {
+//           e.preventDefault();
+//           var param = $(this).attr("href").replace("#","");
+//           var concept = $(this).text();
+//           $('.search-panel span#search_concept').text(concept);
+//           $('.input-group #search_param').val(param);
+//          });
+//     });
+// var a = document.getElementByTagName('a').item(0);
+// $(a).on('keyup', function(evt){
+// console.log(evt);
+// if(evt.keycode === 13){
+
+// alert('search?');
+// } 
+// }); 
--- routes/movie.js
+++ routes/movie.js
@@ -5,7 +5,8 @@ const parser = require('../parse');
 
 // Demo File
 router.get('/movie', (req, res, next) => {
-
+    console.log('received a request');
+    console.log(req.body)
     res.send(parser.searchForMovie('shrek'));
 });
 
