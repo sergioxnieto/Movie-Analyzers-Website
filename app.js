5dbdca1a1617d484d8306719562fcee3258d4002
Sergio Nieto <s.nieto.t@gmail.com>
2020-10-21 22:04:43 -0700

    Implementation of a "dummy" route

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
+// Express import and initialization
 const express = require('express');
 const app = express();
 
+// Port to open localhost:{PORT}
 const PORT = 3000;
 
+// Import all the necessary routing files for our API
+const indexRouter = require('./routes/index');
+
+// This hosts our website. Public refers to the public folder
+// in the directory which has the scripts, stylesheet, and html
 app.use(express.static('public'));
 
+
+// Insert the routes which the API will be able to handle
+app.use('/test', indexRouter);    // Demo route
+
+
+// This is what keeps the site listening for events
 app.listen(PORT, () => {
     console.log('App running...');
     console.log(`Listening on ${PORT}`);
